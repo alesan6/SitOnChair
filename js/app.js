a47dbe2e6f6a2f1f1fa0f50bf0a2f839efe8efc9
Marek Rychlicki <krychu9@gmail.com>
2017-04-24 09:09:09 +0200

    Calculator - done

--- js/app.js
+++ js/app.js
@@ -65,7 +65,7 @@ rightArrow.addEventListener("click", function (){
     galleryList[indexOfPicture].classList.toggle('visibility');
 });
 
-// application dropdown lists
+// application dropdown lists - hide&visible
 
 var arrowBtn1 = document.getElementById("arrow1");
 var typeList1 = document.getElementById("panel1");
@@ -76,11 +76,13 @@ var clickCounter2 = 0;
 var arrowBtn3 = document.getElementById("arrow3");
 var typeList3 = document.getElementById("panel3");
 var clickCounter3 = 0;
+var sectionApp = document.querySelector(".application");
+
 
 arrowBtn1.addEventListener("click", function (){
-    clickCounter1 += 1;
+    clickCounter2 += 1;
 
-    if (clickCounter1 %2 !==0) {
+    if (clickCounter2 %2 !==0) {
         typeList1.style.visibility="visible";
     } else {
         typeList1.style.visibility="hidden";
@@ -103,6 +105,76 @@ arrowBtn3.addEventListener("click", function (){
     } else {
         typeList3.style.visibility="hidden";
     }
+});
+// sectionApp.addEventListener("click", function (){
+//     if (typeList1.style.visibility="visible") {
+//     typeList1.style.visibility="hidden";
+//     }
+//     // typeList2.style.visibility="hidden";
+//     // typeList3.style.visibility="hidden";
+// });
+
+
+//calculator
+
+var panelLeftTitle = document.querySelector(".panel_left .title");
+var panelLeftColor = document.querySelector(".panel_left .color");
+var panelLeftPattern = document.querySelector(".panel_left .pattern");
+var panelLeftTransport = document.querySelector(".panel_left .transport");
+var panelRightTitleValue = document.querySelector(".panel_right .valueRight");
+var panelRightColorValue = document.querySelector(".panel_right .color");
+var panelRightPatternValue = document.querySelector(".panel_right .pattern");
+var panelRightransportValue = document.querySelector(".panel_right .transport");
+var checkTransport = document.querySelector("#transport");
+var sumElement = document.querySelector(".sum strong");
+
+
+function calculateSum() {
+    var summary = 0;
+    var sumArray = [parseInt(panelRightTitleValue.innerHTML), parseInt(panelRightColorValue.innerHTML),
+        parseInt(panelRightPatternValue.innerHTML), parseInt(panelRightransportValue.innerHTML)];
+
+    for (var i = 0; i < sumArray.length; i++) {
+        if (!isNaN(sumArray[i])) {
+            summary += sumArray[i]
+        }
+        sumElement.innerHTML = summary;
+    }
+}
+
+for (var i = 0; i < typeList1.children.length; i++) {
+    typeList1.children[i].addEventListener('click', function() {
+        panelLeftTitle.innerHTML = this.innerHTML;
+        panelRightTitleValue.innerHTML = this.dataset.price;
+        calculateSum();
+    });
+}
+
+for (var i = 0; i < typeList2.children.length; i++) {
+    typeList2.children[i].addEventListener('click', function() {
+        panelLeftColor.innerHTML = this.innerHTML;
+        panelRightColorValue.innerHTML = this.dataset.color;
+        calculateSum();
+    });
+}
+
+for (var i = 0; i < typeList3.children.length; i++) {
+    typeList3.children[i].addEventListener('click', function() {
+        panelLeftPattern.innerHTML = this.innerHTML;
+        panelRightPatternValue.innerHTML = this.dataset.pattern;
+        calculateSum();
+    });
+}
+
+checkTransport.addEventListener('click', function() {
+    if (checkTransport.checked == true) {
+        panelLeftTransport.innerHTML = this.nextElementSibling.innerHTML;
+        panelRightransportValue.innerHTML = this.dataset.transportPrice;
+    } else {
+        panelLeftTransport.innerHTML = "";
+        panelRightransportValue.innerHTML = "";
+    }
+    calculateSum();
 });
 
 });
